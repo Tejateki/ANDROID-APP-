f778c6ff083f4b82ac01f9ad5f83aec6488739ab
TEJA TEKI <134042968+Tejateki@users.noreply.github.com>
2024-06-12 18:06:28 +0530

    LOGIN_REG

---
+++ login_reg/app/src/main/java/com/example/login_reg/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.login_reg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.net.PasswordAuthentication;
+
+public class MainActivity extends AppCompatActivity {
+    private Button submit;
+    private EditText name,email,Studentid;
+    private PasswordAuthentication password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name=findViewById(R.id.button);
+        email=findViewById(R.id.)
+
+
+    }
+}
